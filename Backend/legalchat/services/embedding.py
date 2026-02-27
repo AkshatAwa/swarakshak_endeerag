d28a8033284d851e968d3cb2e08742585a035934
AkshatAwasthi <137002607+AkshatAwa@users.noreply.github.com>
2026-02-27 21:17:25 +0530

    Add embedding function to retrieve embeddings

--- Backend/legalchat/services/embedding.py
+++ Backend/legalchat/services/embedding.py
@@ -1,1 +1,15 @@
+import requests
 
+OLLAMA_URL = "http://localhost:11434/api/embeddings"
+EMBED_MODEL = "mxbai-embed-large"
+
+def get_embedding(text: str):
+    res = requests.post(
+        OLLAMA_URL,
+        json={
+            "model": EMBED_MODEL,
+            "prompt": text
+        }
+    )
+    res.raise_for_status()
+    return res.json()["embedding"]
